6fcce9500f54e0a6b8b9510a19a651feb6618734
Caiosilva8 <caiosilva2207999@gmail.com>
2019-07-12 21:41:23 -0300

    ProjFinalAula14

--- src/app/finalizar-compra/finalizar-compra.page.ts
+++ src/app/finalizar-compra/finalizar-compra.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-finalizar-compra',
@@ -7,9 +12,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalizarCompraPage implements OnInit {
 
-  constructor() { }
+  idUsuario : string;
+  //finalizar : FinalizarCompraPage = new FinalizarCompraPage();
+  firestore = firebase.firestore();
+  settings = { timestampsInSnapshots: true };
+  formGroup : FormGroup;
+
+  constructor(public formBuilder : FormBuilder,
+              public toastController : ToastController,
+              public router : Router,
+              public loadingController : LoadingController,
+              public firebaseauth : AngularFireAuth) {
+
+                this.firebaseauth.authState.subscribe(obj=>{
+                  this.idUsuario = this.firebaseauth.auth.currentUser.uid;
+
+                  let ref = this.firestore.collection('finalizar/').doc(this.idUsuario)
+                  ref.get().then(doc=>{
+                    this
+                  })
+                })
+    this.formulario();
+   }
 
   ngOnInit() {
   }
 
+  formulario(){
+    this.formGroup = this.formBuilder.group({
+      cep : [],
+      cpf : [],
+      identidade : [],
+      celular : [],
+      bairro : [],
+      endereco : [],
+      numero : [],
+      complemento : [],
+      pontoR : []
+    })
+  }
+
+  atualizar(){
+    let ref = this.firestore.collection('finalizar')
+    ref.doc(this.idUsuario).set(this.formGroup.value)
+    .then(()=>{
+      this.toast('Atualizado com sucesso');
+      //this.router.navigate(['']);
+      this.loadingController.dismiss();
+    }).catch(()=>{
+      this.toast('Erro ao Atualizar')
+    })
+  }
+
+  async toast(msg : string) {
+    const toast = await this.toastController.create({
+      message: 'Cadastrado com sucesso!',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async loading() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando',
+      duration: 2000
+    });
+    await loading.present();
+  }
 }
